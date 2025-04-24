37c0e25f9abe1fcec248a569d4b82b2f83bcdc6e
gusvargas <gustavovargas11m@gmail.com>
2025-04-24 18:50:11 -0500

    Agrega funcion mostrar_menu con opciones de la tienda

---
+++ src/main.py
@@ -0,0 +1,14 @@
+def mostrar_menu():
+    print("""Bienvenido a la tienda virtual 🛍️
+¿Qué deseas hacer?
+
+1. Ver catálogo
+2. Agregar producto al carrito
+3. Eliminar producto del carrito
+4. Vaciar carrito
+5. Mostrar carrito
+6. Finalizar compra
+7. Salir
+    """)
+
+mostrar_menu()
