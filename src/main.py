3d5f0c1c147def4fd06e05afee349e09ed7d9172
gusvargas <gustavovargas11m@gmail.com>
2025-04-24 22:43:18 -0500

    Agrega función ver_catalogo para mostrar productos disponibles

--- src/main.py
+++ src/main.py
@@ -1,3 +1,21 @@
+bodega = [
+    {
+        "codigo": "A001",
+        "nombre": "Pan",
+        "precio": 1.50
+    },
+    {
+        "codigo": "B001",
+        "nombre": "Leche",
+        "precio": 3.50
+    },
+    {
+        "codigo": "C001",
+        "nombre": "Chocolate",
+        "precio": 1.80
+    }
+]
+
 def mostrar_menu():
     print("""Bienvenido a la tienda virtual 🛍️
 ¿Qué deseas hacer?
@@ -11,4 +29,10 @@ def mostrar_menu():
 7. Salir
     """)
 
-mostrar_menu()
+def ver_catalogo():
+    print(f'{"Código":<15} | {"Producto":<15} | {"Precio":<6}')
+    print("-" * 42)
+    for producto in bodega:
+        print(f'Código: {producto["codigo"]:<7} | {producto["nombre"]:<15} | S/{producto["precio"]:<6}')
+
+ver_catalogo()
